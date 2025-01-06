a2b3f50e8fe121be9e214971b15d5e164ba1a426
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-01-06 15:31:56 -0500

    Added more tests for TestStripCodes

--- ansi_test.go
+++ ansi_test.go
@@ -156,8 +156,15 @@ func TestStripCodes(t *testing.T) {
 		expected string
 	}{
 		{"\x1b[31mHello\x1b[0m", "Hello"},
-		{"\033[31mHello\033[0m", "Hello"},
-		{"\u001b[31mHello\u001b[0m", "Hello"},
+		{"\x1b[1;32mBold Green\x1b[0m", "Bold Green"},
+		{"\x1b[4;33mUnderlined Yellow\x1b[0m", "Underlined Yellow"},
+		{"\x1b[38;5;82m256 Color Green\x1b[0m", "256 Color Green"},
+		{"\x1b[48;2;255;0;0mTrue Color Red Background\x1b[0m", "True Color Red Background"},
+		{"\x1b]0;Title\x07", "Title\x07"}, // OSC (Operating System Command) sequence
+		{"\x1b[?25lHidden Cursor\x1b[?25h", "Hidden Cursor"},
+		{"\x1b[2J\x1b[HClear Screen", "Clear Screen"},
+		{"\x1b[1;31;42mMultiple Attributes\x1b[0m", "Multiple Attributes"},
+		{"\x1b[0mReset\x1b[0m", "Reset"},
 	}
 
 	for _, test := range tests {
