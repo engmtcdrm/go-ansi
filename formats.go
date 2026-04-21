a6fd2b30321bd42dd5715394aa72458da002c255
John <deedsoftheforsaken@gmail.com>
2026-04-20 20:58:11 -0400

    chore: renamed variables and added more

--- formats.go
+++ formats.go
@@ -10,10 +10,15 @@ const (
 	Underline       = csi + "4m"  // Underlined text.
 	SlowBlink       = csi + "5m"  // Slow blinking text.
 	RapidBlink      = csi + "6m"  // Rapid blinking text.
-	Reverse         = csi + "7m"  // Reversed colors.
-	Hidden          = csi + "8m"  // Hidden text.
+	Reverse         = csi + "7m"  // Reversed colors. Not widely supported.
+	Hide            = csi + "8m"  // Hide text. Not widely supported.
 	Strike          = csi + "9m"  // Strikethrough text.
 	DoubleUnderline = csi + "21m" // Double underlined text.
+
+	// Hidden text. Not widely supported.
+	//
+	// Deprecated: use [Hide] instead. This will be removed in v2.0.0.
+	Hidden = Hide
 
 	// Faint, decreased intensity, or dim.
 	//
@@ -27,6 +32,18 @@ const (
 	ResetUnderline = csi + "24m" // Reset underline.
 	ResetBlink     = csi + "25m" // Reset blink.
 	ResetReverse   = csi + "27m" // Reset reverse.
-	ResetHidden    = csi + "28m" // Reset hidden.
+	ResetHide      = csi + "28m" // Reset hide.
 	ResetStrike    = csi + "29m" // Reset strikethrough.
+
+	// Reset Hidden text. Not widely supported.
+	//
+	// Deprecated: use [ResetHide] instead. This will be removed in v2.0.0.
+	ResetHidden = ResetHide
+
+	Framed         = csi + "51m" // Framed text.
+	Encircled      = csi + "52m" // Encircled text.
+	Overlined      = csi + "53m" // Overlined text.
+	ResetFramed    = csi + "54m" // Reset framed and encircled.
+	ResetOverlined = csi + "55m" // Reset overlined.
+	IdeogramRight  = csi + "73m" // Ideogram right underline.
 )
