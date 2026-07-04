0ea5c28c68d247f19a77b21faf3df9dd997486a7
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-07-04 16:06:42 +0000

    chore: Removed StripCodes function

--- ansi.go
+++ ansi.go
@@ -43,13 +43,6 @@ func StripRunes[T ~[]rune](input T) T {
 	return T(stripedRunes)
 }
 
-// StripCodes removes all ANSI escape sequences from the input string.
-//
-// Deprecated: Use [Strip] instead. This will be removed in v2.0.0.
-func StripCodes(input string) string {
-	return Strip(input)
-}
-
 // colorInRange checks if the color value is between 0 and 255.
 func colorInRange(color int) bool {
 	return color >= 0 && color <= 255
