db625e8d47af3a8a59aa0704892a1d2de258a0d1
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-07-04 14:34:25 -0400

    chore: Reorganized functions

--- ansi.go
+++ ansi.go
@@ -24,25 +24,6 @@ func Strip[T ~string | ~[]byte](input T) T {
 	return T(stripedString)
 }
 
-// Strip8Bit removes all 8-bit C1 ANSI escape sequences from the input.
-func Strip8Bit[T ~string | ~[]byte](input T) T {
-	var stripedString []byte
-
-	for len(input) > 0 {
-		ansiLen := escapeLength8Bit(input)
-
-		if ansiLen == 0 {
-			stripedString = append(stripedString, input[0])
-			input = input[1:]
-			continue
-		}
-
-		input = input[ansiLen:]
-	}
-
-	return T(stripedString)
-}
-
 // StripRunes removes all 7-bit ANSI escape sequences from input.
 func StripRunes[T ~[]rune](input T) T {
 	var stripedRunes []rune
@@ -60,6 +41,25 @@ func StripRunes[T ~[]rune](input T) T {
 	}
 
 	return T(stripedRunes)
+}
+
+// Strip8Bit removes all 8-bit C1 ANSI escape sequences from the input.
+func Strip8Bit[T ~string | ~[]byte](input T) T {
+	var stripedString []byte
+
+	for len(input) > 0 {
+		ansiLen := escapeLength8Bit(input)
+
+		if ansiLen == 0 {
+			stripedString = append(stripedString, input[0])
+			input = input[1:]
+			continue
+		}
+
+		input = input[ansiLen:]
+	}
+
+	return T(stripedString)
 }
 
 // Strip8BitRunes removes all 8-bit C1 ANSI escape sequences from the input.
